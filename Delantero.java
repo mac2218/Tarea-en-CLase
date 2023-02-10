969eb82bb68c08e31b0801c593bd368efa31f9e9
MarcosDiaz1409 <dia221102@uvg.edu.gt>
2023-02-10 08:13:32 -0600

    Cambios

--- Delantero.java
+++ Delantero.java
@@ -3,19 +3,22 @@ public class Delantero extends EquipoFut {
     private int cantPases;
     private int golesAnotados;
     private int golesFallados;
+    private int faltas;
 
     public Delantero(){
         super();
         cantPases = 0;
         golesAnotados = 0;
         golesFallados = 0;
+        faltas = 0;
     }
     
-    public Delantero(String nombre, String apellido, int numJugador, int cantPases, int golesAnotados, int golesFallados){
+    public Delantero(String nombre, String apellido, int numJugador, int cantPases, int golesAnotados, int golesFallados, int faltas){
         super(nombre, apellido, numJugador);
         this.cantPases = cantPases;
         this.golesAnotados = golesAnotados;
         this.golesFallados = golesFallados;
+        this.faltas = faltas;
 
     }
 
