0ad33c721c8d1096c3aa1aba112c8cd00063afa6
MarcosDiaz1409 <dia221102@uvg.edu.gt>
2023-02-10 07:57:13 -0600

    Clase padre

---
+++ EquipoFut.java
@@ -0,0 +1,52 @@
+public abstract class EquipoFut{
+    
+    private String nombre;
+    private String apellido;
+    private int numJugador;
+
+    public EquipoFut(){
+        nombre = "";
+        apellido = "";
+        numJugador = 0;
+    }
+
+    public EquipoFut(String nombre, String apellido, int numJugador){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numJugador = numJugador;
+    }
+
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumJugador() {
+        return this.numJugador;
+    }
+
+    public void setNumJugador(int numJugador) {
+        this.numJugador = numJugador;
+    }
+
+    @Override
+    public String toString(){
+        return "{" +
+            " Nombre:'" + getNombre() + "'" +
+            ", Apellido:'" + getApellido() + "'" +
+            ", Numero de jugador:'" + getNumJugador() + "'" +
+            "}";
+    }
+}
