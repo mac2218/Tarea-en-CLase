68faa547083a866fb5fea5d6b6e7b0bb20013eb4
mac2218 <mac22118@uvg.edu.gt>
2023-02-10 08:29:46 -0600

    Clase Portero

--- Portero.java
+++ Portero.java
@@ -1,22 +1,22 @@
 public class Portero extends EquipoFut{
-    private int cantPases;
+    private int porteriasCero;
     private int golesEncajados;
     private int golesAtajados;
-    private int faltas;
+    
 
     public Portero(){
         super();
-        cantPases = 0;
-        golesAnotados = 0;
-        golesFallados = 0;
-        faltas = 0;
+        porteriasCero = 0;
+        golesEncajados = 0;
+        golesAtajados = 0;
+        
     }
     
-    public Portero(String nombre, String apellido, int numJugador, int cantPases, int golesAnotados, int golesFallados, int faltas){
+    public Portero(String nombre, String apellido, int numJugador, int porteriasCero, int golesEncajados, int golesAtajados){
         super(nombre, apellido, numJugador);
-        this.cantPases = cantPases;
-        this.golesAnotados = golesAnotados;
-        this.golesFallados = golesFallados;
+        this.porteriasCero = porteriasCero;
+        this.golesEncajados = golesEncajados;
+        this.golesAtajados = golesAtajados;
         this.faltas = faltas;
 
     }
@@ -47,4 +47,4 @@ public class Portero extends EquipoFut{
 }
 
     
-}
+
