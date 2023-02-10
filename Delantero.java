eb18f4adae1a603d9a7ec342dcb37b46a5e95425
mac2218 <mac22118@uvg.edu.gt>
2023-02-10 08:11:40 -0600

    Delantero

---
+++ Delantero.java
@@ -0,0 +1,22 @@
+public class Delantero extends EquipoFut {
+    
+    private int cantPases;
+    private int golesAnotados;
+    private int golesFallados;
+
+    public Delantero(){
+        super();
+        cantPases = 0;
+        golesAnotados = 0;
+        golesFallados = 0;
+    }
+    
+    public Delantero(String nombre, String apellido, int numJugador, int cantPases, int golesAnotados, int golesFallados){
+        super(nombre, apellido, numJugador);
+        this.cantPases = cantPases;
+        this.golesAnotados = golesAnotados;
+        this.golesFallados = golesFallados;
+
+    }
+    
+}
